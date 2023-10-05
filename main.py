182c314ccd6e451f2e420809565778b3f501c37f
Thirumaliyyappan <145108414+Thirumaliyyappan@users.noreply.github.com>
2023-10-05 14:45:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def factorial(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects for a Batsman and a Bowler
+batsman1 = Batsman()
+bowler1 = Bowler()
+
+# Call the play() method for each object
+batsman1.play()  # The batsman is batting.
+bowler1.play()   # The bowler is bowling.
